c0455a4b7eb7377381618674c11740a49bc2875d
Thanwarat Munkam <53259275+Kmunkam@users.noreply.github.com>
2021-01-27 19:26:06 +0700

    THup

---
+++ src/views/Summary1.vue
@@ -0,0 +1,207 @@
+<template>
+  <div >
+    <SummaryHead/>
+    
+    <div class="summary">    
+        <b-container fluid>
+        
+          <!-- สาขาวิชา -->
+          <div class="mt-5 mx-3" id="branch" to="/src/views/Summary2.vue">
+            <b-row class="mt-3 d-flex align-items-center">              
+              <b-col cols="4" class="border border-dark rounded-left p-2">
+                  <div class="d-flex flex-wrap justify-content-around">  
+                    <div class="d-flex align-items-center">
+                      <h4 class="border border-dark rounded p-2">CoE</h4>
+                    </div>
+                    <div class="">
+                      <p class="mb-0"><small>โครงการย่อยที่กำลังดำเนินการ</small></p>
+                      <h2 class="mb-0 text-center">23</h2>
+                      <p class="text-muted mt-0 mb-0 float-right"><small>โครงการ</small></p>
+                    </div>
+                    <div class="">
+                      <p class="mb-0"><small>โครงการย่อยที่เสร็จสิ้น</small></p>
+                      <h2 class="mb-0 text-center">23</h2>
+                      <p class="text-muted mt-0 mb-0 float-right"><small>โครงการ</small></p>
+                    </div>
+                    
+                  </div>  
+              </b-col> 
+                        
+              <b-col cols="8" class="border border-dark rounded-right p-2 ">
+                <div class="d-flex flex-wrap justify-content-around">                  
+                   <div class="">
+                    <p class="mb-0"><small>งบประมาณทั้งหมด (ตามแผน)</small></p>
+                    <h2 class="mb-0 text-center">8300</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0"><small>เบิกจ่ายทั้งหมด</small></p>
+                    <h2 class="mb-0 text-center">5000</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0 "><small>งบประมาณทคงเหลือ (ตามแผน)</small></p>
+                    <h2 class="mb-0 text-center">4500</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0"><small>งบประมาณทคงเหลือ (เบิกจ่ายจริง)</small></p>
+                    <h2 class="mb-0 text-center">4500</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                </div> 
+              </b-col>
+            </b-row>
+          </div>
+
+          <div class="mt-5 mx-3" id="branch">
+            <b-row class="mt-3 d-flex align-items-center">              
+              <b-col cols="4" class="border border-dark rounded-left p-2">
+                  <div class="d-flex flex-wrap justify-content-around">  
+                    <div class="d-flex align-items-center">
+                      <h4 class="border border-dark rounded p-2">EE</h4>
+                    </div>
+                    <div class="">
+                      <p class="mb-0"><small>โครงการย่อยที่กำลังดำเนินการ</small></p>
+                      <h2 class="mb-0 text-center">23</h2>
+                      <p class="text-muted mt-0 mb-0 float-right"><small>โครงการ</small></p>
+                    </div>
+                    <div class="">
+                      <p class="mb-0"><small>โครงการย่อยที่เสร็จสิ้น</small></p>
+                      <h2 class="mb-0 text-center">23</h2>
+                      <p class="text-muted mt-0 mb-0 float-right"><small>โครงการ</small></p>
+                    </div>
+                    
+                  </div>  
+              </b-col> 
+                        
+              <b-col cols="8" class="border border-dark rounded-right p-2 ">
+                <div class="d-flex flex-wrap justify-content-around">                  
+                   <div class="">
+                    <p class="mb-0"><small>งบประมาณทั้งหมด (ตามแผน)</small></p>
+                    <h2 class="mb-0 text-center">8300</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0"><small>เบิกจ่ายทั้งหมด</small></p>
+                    <h2 class="mb-0 text-center">5000</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0 "><small>งบประมาณทคงเหลือ (ตามแผน)</small></p>
+                    <h2 class="mb-0 text-center">4500</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0"><small>งบประมาณทคงเหลือ (เบิกจ่ายจริง)</small></p>
+                    <h2 class="mb-0 text-center">4500</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                </div> 
+              </b-col>
+            </b-row>
+          </div>
+
+          <div class="mt-5 mx-3" id="branch">
+            <b-row class="mt-3 d-flex align-items-center">              
+              <b-col cols="4" class="border border-dark rounded-left p-2">
+                  <div class="d-flex flex-wrap justify-content-around">  
+                    <div class="d-flex align-items-center">
+                      <h4 class="border border-dark rounded p-2">EE</h4>
+                    </div>
+                    <div class="">
+                      <p class="mb-0"><small>โครงการย่อยที่กำลังดำเนินการ</small></p>
+                      <h2 class="mb-0 text-center">23</h2>
+                      <p class="text-muted mt-0 mb-0 float-right"><small>โครงการ</small></p>
+                    </div>
+                    <div class="">
+                      <p class="mb-0"><small>โครงการย่อยที่เสร็จสิ้น</small></p>
+                      <h2 class="mb-0 text-center">23</h2>
+                      <p class="text-muted mt-0 mb-0 float-right"><small>โครงการ</small></p>
+                    </div>
+                    
+                  </div>  
+              </b-col> 
+                        
+              <b-col cols="8" class="border border-dark rounded-right p-2 ">
+                <div class="d-flex flex-wrap justify-content-around">                  
+                   <div class="">
+                    <p class="mb-0"><small>งบประมาณทั้งหมด (ตามแผน)</small></p>
+                    <h2 class="mb-0 text-center">8300</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0"><small>เบิกจ่ายทั้งหมด</small></p>
+                    <h2 class="mb-0 text-center">5000</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0 "><small>งบประมาณทคงเหลือ (ตามแผน)</small></p>
+                    <h2 class="mb-0 text-center">4500</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                  <div class="">
+                    <p class="mb-0"><small>งบประมาณทคงเหลือ (เบิกจ่ายจริง)</small></p>
+                    <h2 class="mb-0 text-center">4500</h2>
+                    <p class="text-muted mt-0 mb-0 float-right"><small>บาท</small></p>
+                  </div>
+                </div> 
+              </b-col>
+            </b-row>
+          </div>
+
+        </b-container>     
+      </div>
+
+  </div>
+</template>
+
+<script>
+import SummaryHead from "@/components/SummaryHead.vue"
+
+export default {
+  name: "summary2",
+    components :{
+        SummaryHead
+    },
+    data() {
+      return {
+        selectedYear: "2564",
+        optionsYear: [
+          { value: '2564', text: '2564' },
+          { value: '2563', text: '2563' },
+          { value: '2562', text: '2562' },
+          { value: '2561', text: '2561' }
+        ]
+      }
+    },
+
+}
+
+</script>
+
+<style>
+
+.summary{
+  margin: 20px;
+  margin-left: 30px;
+  margin-right: 30px;
+  
+}
+.baht{
+  float: right;
+  margin-bottom: 0px;
+  margin-top: 0px;
+
+}
+#border{
+  display: block;
+  padding: 10px;  
+  border: 0.3px solid black;
+  border-radius: 5px;
+  width: 210px;
+
+}
+
+
+</style>
---
+++ src/views/Summary2.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <SummaryHead />
+        <div class="summary2">
+            <h5 class="pt-2 ml-4 pb-0">โครงการทั้งหมด</h5>
+            <div ref="table" class="table-sty"></div>
+        </div>
+
+    </div>
+  
+</template>
+
+<script>
+import SummaryHead from "@/components/SummaryHead.vue"
+import Tabulator from 'tabulator-tables'; 
+
+export default {
+    name: "summary2",
+    components :{
+        SummaryHead
+    },
+    data() {
+      return {
+        modalShow: false,
+        tabulator: null, //variable to hold your table
+        tableData: [
+            {name: 'โครงการหลักA',strategic_issue: "2",strategic: "1",strategy: "1", 
+            _children:[
+                {name:"โครงการย่อยA1",  strategic_issue: "1",strategic: "2",strategy: "3",},
+                {name:"โครงการย่อยA2",  strategic_issue: "1",strategic: "2",strategy: "3",}],
+             },
+            {name: 'โครงการหลักB',strategic_issue: "1",strategic: "3",strategy: "1",
+            _children:[
+                {name:"โครงการย่อยB1", budged: 1500,},
+                {name:"โครงการย่อยB2", strategic_issue: "1", strategic: "2", strategy: "3", budged: 2000,}],
+            },
+            {name: 'โครงการหลักC',strategic_issue: "4",strategic: "1",strategy: "2",},
+ 
+        ],//data for table to display
+
+
+        }
+  },
+   watch:{
+    //update table if data changes
+    tableData:{
+      handler: function (newData) {
+        this.tabulator.replaceData(newData);
+      },
+      deep: true,
+    }
+  },mounted(){
+    
+    //instantiate Tabulator when element is mounted
+    this.tabulator = new Tabulator(this.$refs.table, {
+        data: this.tableData, //link data to table
+        dataTree:true,
+        dataTreeStartExpanded:true,
+        dataTreeChildIndent : 20, //indent child rows
+        
+        columns: [
+            {title:"ชื่อโครงการ", field:"name", width:200, responsive:0,},
+            {title:"ประเด็นยุทธศาสตร์", field:"strategic_issue", width:200, hozAlign:"right"},
+            {title:"ยุทธศาสตร์", field:"strategic", width:180,hozAlign:"right",},
+            {title:"กลยุทธ", field:"strategy", width:180,hozAlign:"right"},
+         
+        ],
+         
+
+    })},
+    template: '<div ref="table"></div>', //create table holder element
+
+}
+</script>
+
+<style lang="scss">
+@import  "~vue-tabulator/dist/scss/bootstrap/tabulator_bootstrap4.scss";
+
+.summary2{
+    margin: 20px;
+    margin-left: 40px;
+    margin-right: 40px;
+    border: 1px solid black;
+    border-radius: 10px;
+    background-color: white;
+    
+}
+.table-sty{
+    margin: 10px;
+}
+h4{
+     margin: 10px;
+}
+</style>
