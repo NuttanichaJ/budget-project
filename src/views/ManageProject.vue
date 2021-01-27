82eecb060382c6f6ce2dc907de7e97aebc19f3b7
nuttanichajirapisitkul <nuttanicha_j@github.com>
2021-01-28 03:18:11 +0700

    searchP

--- src/views/ManageProject.vue
+++ src/views/ManageProject.vue
@@ -6,9 +6,9 @@
         <b-navbar-nav class="mb-2 mr-sm-2 mb-sm-0 ml-auto">
                 <b-nav-form>
                     <b-input-group >
-                        <b-form-input placeholder="ค้นหา"></b-form-input>
+                        <b-form-input placeholder="ค้นหา" id='search'></b-form-input>
                         <b-input-group-append>
-                            <b-button class="mb-2 mr-sm-2 mb-sm-0"><font-awesome-icon icon="search" /></b-button>                        
+                            <b-button @click='search' class="mb-2 mr-sm-2 mb-sm-0"><font-awesome-icon icon="search" /></b-button>                        
                         </b-input-group-append>
                         <b-button class="mb-2 mr-sm-2 mb-sm-0" @click='save'>บันทึก</b-button>
                         <b-button class="mb-2 mr-sm-0 mb-sm-0" @click='cancel'>ยกเลิก</b-button>
@@ -195,6 +195,13 @@ export default {
     addRow() {
       this.tabulator.addRow({});
     },
+
+    search() {
+      var valueEl = document.getElementById("search");
+      if(valueEl != '') {
+        this.tabulator.setFilter('name','regex', valueEl.value);
+      }
+    }
   },
 };
 
