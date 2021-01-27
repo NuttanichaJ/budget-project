10503934641a6eaaf4676569865da2e6f0b1f483
Thanwarat Munkam <53259275+Kmunkam@users.noreply.github.com>
2021-01-27 19:02:53 +0700

    Add files via upload

---
+++ src/views/adminManagement.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <b-nav class="mt-3" id="menu">
+        <b-button id="add-user" class="my-2" @click='addRow'>เพิ่มสมาชิก</b-button>
+
+        <b-dropdown id="dropdown-left" text="สิทธิ์การใช้งาน" class="m-2">
+            <b-dropdown-item>ผู้บริหาร</b-dropdown-item>
+            <b-dropdown-item>ส่วนกลาง</b-dropdown-item>
+            <b-dropdown-item>สาขาวิชา</b-dropdown-item>        
+        </b-dropdown>        
+        
+            <b-navbar-nav class="mt-2 ml-auto">
+                <b-nav-form>
+                    <b-input-group >
+                        <b-form-input placeholder="ค้นหา"></b-form-input>
+                        <b-input-group-append>
+                            <b-button><font-awesome-icon icon="search" /></b-button>                        
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-nav-form>        
+            </b-navbar-nav>
+        
+    </b-nav>
+    <div ref="table" class="table-sty"></div>
+
+     
+
+  </div>
+</template>
+
+<script>
+import Tabulator from 'tabulator-tables'; 
+export default {
+  name: "ManageUser",
+  data() {
+      return {
+        modalShow: false,
+        tabulator: null, //variable to hold your table
+        tableData: [{
+          name: 'Teste',
+            location: "this place",
+            progress: 100,
+        },{
+            name: 'Nurse',
+            location: "this place",
+            progress: 12,
+        
+        }], //data for table to display
+      }
+  },
+   watch:{
+    //update table if data changes
+    tableData:{
+      handler: function (newData) {
+        this.tabulator.replaceData(newData);
+      },
+      deep: true,
+    }
+  },
+  mounted(){   
+    //instantiate Tabulator when element is mounted
+    this.tabulator = new Tabulator(this.$refs.table, {
+      data: this.tableData, //link data to table
+      addRowPos: "bottom",
+      layout:"fitColumns",
+      columns: [
+        {title:"ชื่อ - นามสกุล", field:"name", width:200, editor:"input"},
+        {title:"E-mail", field:"mail", headerSort:false, width:250, editor:"input"},
+        {title:"ฝ่าย / สาขาวิชา", field:"branch", width:180, editor:"select", editorParams:{values:{"วิศวกรรมคอมพิวเตอร์":"วิศวกรรมคอมพิวเตอร์", "วิศวกรรมโยธา":"วิศวกรรมโยธา", "วิศวกรรมไฟฟ้า":"วิศวกรรมไฟฟ้า"}}},
+        {title:"สิทธิ์การใช้งาน", field:"permission", width:180, editor:"select", editorParams:{values:{"ผู้บริหาร":"ผู้บริหาร", "ส่วนกลาง":"ส่วนกลาง", "สาขาวิชา":"สาขาวิชา"}}},
+        {formatter:delUser, width:80, align:"center", frozen:true, headerSort:false, 
+          //cellClick:del() 
+          }
+        ], //define table columns
+    });
+
+  },
+
+  template: '<div ref="table"></div>', //create table holder element
+
+  mounted(){
+    var delUser = function(cell, formatterParams){ //plain text value
+        //cell, formatterParams, onRendered;
+        return '<b-button @click="addRow">ลบ</b-button>';}
+    //instantiate Tabulator when element is mounted
+    this.tabulator = new Tabulator(this.$refs.table, {
+      data: this.tableData, //link data to table
+      addRowPos:"bottom",
+      columns: [
+        {title:"ชื่อ - นามสกุล", field:"name", width:200, editor:"input"},
+        {title:"E-mail", field:"mail", headerSort:false, width:250, editor:"input"},
+        {title:"ฝ่าย / สาขาวิชา", field:"branch", width:180, editor:"select", editorParams:{values:{"วิศวกรรมคอมพิวเตอร์":"วิศวกรรมคอมพิวเตอร์", "วิศวกรรมโยธา":"วิศวกรรมโยธา", "วิศวกรรมไฟฟ้า":"วิศวกรรมไฟฟ้า"}}},
+        {title:"สิทธิ์การใช้งาน", field:"permission", width:180, editor:"select", editorParams:{values:{"ผู้บริหาร":"ผู้บริหาร", "ส่วนกลาง":"ส่วนกลาง", "สาขาวิชา":"สาขาวิชา"}}},
+        {formatter:delUser, width:80, align:"center", frozen:true, headerSort:false, 
+          //rowClick:del(),
+          }
+          ,], //define table columns
+    });
+   
+
+  },
+  
+ methods: {
+    del(e, cell) {
+      this.$confirm({
+        message: 'ต้องการลบข้อมูลของ ' + cell.getRow().getData().name + 'ใช่หรือไม่',
+        button: {
+          yes: 'ตกลง',
+          no: 'ยกเลิก'
+        }
+      })
+    },
+
+    addRow() {
+      this.tabulator.addRow({});
+    },
+    
+    delRow() {
+      this.tabulator.deleteRow([1]);
+    },
+
+       
+  },
+  
+};
+
+</script>
+
+<style lang="scss">
+@import  "~vue-tabulator/dist/scss/bootstrap/tabulator_bootstrap4";
+
+#menu {
+    padding: 10px;
+    padding-right: 50px;
+}
+#add-user{
+    background-color: #84a856;
+    margin-left: 30px;
+    
+}
+.table-sty{
+  padding: 30px;
+  padding-top: 10px;
+}
+
+</style>
