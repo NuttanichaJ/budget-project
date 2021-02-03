76676bf81b0ffc3d7c86ff219eb0ba90a693a9f0
Thanwarat Munkam <Kmunkam@gmail.com>
2021-02-04 01:07:38 +0700

    summaryHead update print

--- src/views/ManageProjectC.vue
+++ src/views/ManageProjectC.vue
@@ -118,10 +118,7 @@ export default {
     //instantiate Tabulator when element is mounted
     var table = new Tabulator('#table', {
       data: this.tableData, //link data to table
-<<<<<<< Updated upstream
-      //resizableColumns:false,
-=======
->>>>>>> Stashed changes
+
       addRowPos:"bottom",
       columns: [
         {title:"ชื่อโครงการ", field:"name", width:200, editor:"input", hozAlign:"left", formatter:"textarea", frozen:true, responsive:0, },
@@ -181,7 +178,7 @@ export default {
       })
 
       //add row
-       document.getElementById("add-project").addEventListener("click", function(){
+      document.getElementById("add-project").addEventListener("click", function(){
         table.addRow({});
       });
 
